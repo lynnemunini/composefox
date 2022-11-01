801069a45d09e2b58cccf1bb3bacd04a915056a4
lynnemunini <muninilynne65@gmail.com>
2022-11-01 15:50:45 +0300

    Polish the user interface

--- app/src/main/java/com/example/composefox/MainActivity.kt
+++ app/src/main/java/com/example/composefox/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.example.composefox
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
@@ -20,38 +29,47 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeFoxTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier
-                        .fillMaxSize(),
-                        //.padding(all = 59.dp),
-                    color = MaterialTheme.colors.primary
-                ) {Column() {
-                    Greeting("Android")
-                    showAge()
-                }
-                }
+                MyApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyApp(){
+    Surface(modifier = Modifier
+        .fillMaxHeight()
+        .fillMaxWidth(),
+        color = Color(0xFFFFDDD2)
+    ) {
+        Column(verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+            CreateCircle()
+        }
+    }
 }
 
-@Preview
 @Composable
-fun showAge(age: Int = 12) {
-    Text(text = age.toString())
+fun CreateCircle() {
+    Card(
+        modifier = Modifier
+            .padding(3.dp)
+            .size(105.dp)
+            .clickable {
+                Log.d("Tap", "CreateCircle: Tap")
+            },
+        shape = CircleShape,
+        elevation = 4.dp
+    ) {
+        Box(contentAlignment = Alignment.Center) {
+            Text(text = "Tap", modifier = Modifier)
+        }
+    }
 }
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeFoxTheme {
-        Column() {
-            Greeting("Android")
-            showAge()
-        }
+        MyApp()
     }
 }
